db80f3924b29532fee293517438580df70441a98
Charantej Akri <b.charantej@ajacs.in>
2025-11-16 17:11:44 +0530

    Update UpdateTodoServlet.java

--- src/main/java/com/charan/todo/UpdateTodoServlet.java
+++ src/main/java/com/charan/todo/UpdateTodoServlet.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;  // Add this import
 import java.io.IOException;
 
 @WebServlet("/updateTodo")
