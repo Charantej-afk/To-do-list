89d47ded7fd6351b2b31f87f3d1d08fe0006ea8c
Charantej Akri <b.charantej@ajacs.in>
2025-11-16 17:05:05 +0530

    Update AddTodoServlet.java

--- src/main/java/com/charan/todo/AddTodoServlet.java
+++ src/main/java/com/charan/todo/AddTodoServlet.java
@@ -1,8 +1,11 @@
+package com.charan.todo;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
 @WebServlet("/addTodo")
 public class AddTodoServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String newTodo = request.getParameter("todo");
-        // Add logic to save this to a list or database
-        response.sendRedirect("todos");
-    }
+    // ... your existing code
 }
